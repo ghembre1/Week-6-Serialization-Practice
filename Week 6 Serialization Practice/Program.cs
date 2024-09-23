1a40d7b52a697da51f7193c3e448183091a64d56
Gary Hembree <gbhembree22@amarillocollege.com>
2024-09-23 14:35:16 -0500

    Add project files.

---
+++ Week 6 Serialization Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+public class Person
+{
+public string Name { get; set; }
+    public int Age { get; set; }
+}
+public class Program
+{
+    static void Main()
+    {
+        Person person = new Person { Name = "John", Age = 30 };
+        // Serialize object to JSON
+        string jsonString = JsonSerializer.Serialize(person);
+        Console.WriteLine("Serialized JSON:");
+        Console.WriteLine(jsonString);
+        // Deserialize JSON to object
+        Person deserializedPerson = JsonSerializer.Deserialize<Person>(jsonString);
+        Console.WriteLine($"Deserialized Person: {deserializedPerson.Name},
+    { deserializedPerson.Age}");
+    }
+
+    Console.Read();
+}
